1e8a88f1b73ab49a07af2276e32e35b877951cf8
kaimizzi <kaimizzi@hotmail.com>
2018-11-05 15:34:20 +0100

    Update app/classes/Backend.php
    -Escaped 's' in regular expression which was messing with results
    -Updated slug function to transform slug to lower case

--- app/classes/Backend.php
+++ app/classes/Backend.php
@@ -3,16 +3,16 @@
 namespace App\classes;
 
 class Backend{
-  
+
   public static function generateSlug($slug){
     //remove any character that isnt a space hyphen letter number
     //remove spaces and duplicate hyphens
     //trim left and right from any extra hyphens
 
-    $lettersNumbersSpacesHyphens = '/[^-sa-zA-Z0-9]/';
-    $spacesDuplicateHyphens = '/[-s]+/';
+    $lettersNumbersSpacesHyphens = '/[^-\sa-zA-Z0-9]/';
+    $spacesDuplicateHyphens = '/[-\s]+/';
 
-    $slug = preg_replace($lettersNumbersSpacesHyphens, '', $slug);
+    $slug = preg_replace($lettersNumbersSpacesHyphens, '', strtolower($slug));
     $slug = preg_replace($spacesDuplicateHyphens, '-', $slug);
     $slug = trim($slug, '-');
 
